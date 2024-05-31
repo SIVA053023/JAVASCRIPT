80ded7dc524c0f25dcbc5eae1d24d7b185819ab8
siva krishna <sivakrishna053936@gmail.com>
2024-06-01 00:02:27 +0530

    operator

---
+++ OPERATORS/Relational operators/Shopping.js
@@ -0,0 +1,13 @@
+let productsizes=["L","M","X"]
+let prices=[499,399,599]
+
+/*for(price of prices ){      // this syntax used for to print the all the values of a property
+    console.log(price);
+}*/
+
+if(productsizes=="L" && prices==499){
+    console.log("NOT AVAILABLE");
+}
+else{
+    console.log("PRODUCTS ARE AVAILABLE");
+}
