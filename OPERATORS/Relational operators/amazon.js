25ff4b7697cd19c918e2da510ab41e29e6672993
siva krishna <sivakrishna053936@gmail.com>
2024-05-31 23:48:51 +0530

    operators

---
+++ OPERATORS/Relational operators/amazon.js
@@ -0,0 +1,7 @@
+let productsizes=["L","M","X"]
+let prices=[499,399,599]
+
+for(price of prices ){      // this syntax used for to print the all the values of a property
+    console.log(price)
+}
+
---
+++ OPERATORS/Relational operators/one.js
@@ -0,0 +1,9 @@
+let ename="shiva";
+let esal=52000;
+
+if(ename=="shiva" && esal>50000){
+    console.log("yes he is comfotable in position to survive");
+}
+else{
+    console.log('he is not not in comfortable to survive');
+}
