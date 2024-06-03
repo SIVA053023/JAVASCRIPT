0381758dbb710050544ebbb507f258280632c042
siva krishna <sivakrishna053936@gmail.com>
2024-06-03 22:21:41 +0530

    test

---
+++ Conditional statements/One.js
@@ -0,0 +1,5 @@
+let a=100;
+let b=200;
+let c=300;
+
+a%2===0 ? console.log("this is a even number") :console.log("this is a odd number");
---
+++ Conditional statements/Two.js
@@ -0,0 +1,13 @@
+let a=459;
+let b=599;
+let c=699;
+
+if(a>b && a>c){
+    console.log ("a is the greatest number")
+}
+else if(b>c){
+    console.log("b is the greatest number")
+}
+else{
+    console.log("c is the greatest number")
+}
