a0b251e821008d733dd9036915f5e9a7396db02a
siva krishna <sivakrishna053936@gmail.com>
2024-06-06 17:59:29 +0530

    test

--- DATATYPES/Premitive data types/primitive.js
+++ DATATYPES/Premitive data types/primitive.js
@@ -1,7 +1,7 @@
 // there are two types of datatypes 1.premitive and 2. non- primitive
 // primitive data type means holding single value like numbers,string,boolean,undefined,null, big int,symbol
 
-var price=499; // number
+/*var price=499; // number
 var ename="Amazon"; //string
 var avail=true; //boolean
 var discount; // undefined
@@ -16,4 +16,22 @@ document.write(typeof price);
 document.write(typeof ename);
 document.write(typeof avail);
 document.write(typeof discount);
-document.write(typeof distance_to_moon);
+document.write(typeof distance_to_moon);*/
+var eid=101
+function cal_age(){}
+class Employee{}
+let emp={}
+
+//let typeof="RahulGandhi"
+//let this9 =100
+//let th!s =100
+//let super="Super Start"
+
+let constructor="Hello"
+console.log(constructor)
+
+funciton instanceof(){
+    
+}
+
+
--- OPERATORS/Relational operators/Shopping.js
+++ OPERATORS/Relational operators/Shopping.js
@@ -4,10 +4,20 @@ let prices=[499,399,599]
 /*for(price of prices ){      // this syntax used for to print the all the values of a property
     console.log(price);
 }*/
+          // CASE-1   we get out put "NOT AVAILABLE" Because both logics are true
 
-if(productsizes=="L" && prices==499){
-    console.log("NOT AVAILABLE");
+if(productsizes=="L","M","X" && prices==499,399,599){ 
+    console.log("NOT AVAILABLE");                 
 }
 else{
     console.log("PRODUCTS ARE AVAILABLE");
 }
+      // CASE-2 we get the output "PRODUCTS ARE AVAILABLE" Because the logics are false
+
+      if(productsizes=="L" && prices==499){ 
+        console.log("NOT AVAILABLE");                 
+    }
+    else{
+        console.log("PRODUCTS ARE AVAILABLE");
+    }
+
