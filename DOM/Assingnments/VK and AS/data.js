db564a83699a54061b330f8782acecf58213c17b
siva krishna <sivakrishna053936@gmail.com>
2024-06-12 15:10:18 +0530

    test

---
+++ DOM/Assingnments/VK and AS/data.js
@@ -0,0 +1,14 @@
+function changeimage(){
+    let image=document.getElementById('Siva');
+    console.log(image);
+  image.src='./As.jpg'
+}
+function out(){
+    let image=document.getElementById('Siva');
+    console.log(image);
+    image.src='./vk.webp' 
+    this.style.width= '300px'
+    this.style.height='300px'
+}
+
+
---
+++ DOM/Assingnments/onblur2/data.js
@@ -0,0 +1,6 @@
+function changecase(){
+    let tag_ref=document.getElementById('input')[0];
+    console.log(tag_ref);
+    let 
+
+}
