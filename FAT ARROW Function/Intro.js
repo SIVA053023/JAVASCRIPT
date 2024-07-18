ff8aa889de6c918bc5339266b3a2959efa11ffdc
siva krishna <sivakrishna053936@gmail.com>
2024-07-18 12:47:35 +0530

    tes

--- FAT ARROW Function/Intro.js
+++ FAT ARROW Function/Intro.js
@@ -1,6 +1,6 @@
 // for the fat arrow there is no function keyward and no function name but here we use variable 
 /* ADVANTAGES of Fat arrow
-   1. short and conciese code
+   1. short and concise code
    2.implicit return, means we can avoid {} and return when there is a one statement to print
    3.'this' keyward/pointer pointing to current object (in noramal function object contest is very bigger when we use the 'this' keyward but in case of "FAT ARROW" objrct contest is empty)
    4. Function overriding conficts are resolved */
