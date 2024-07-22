0d14c5873ce651b69e0f992b595a8ce96f8905df
siva krishna <sivakrishna053936@gmail.com>
2024-07-22 13:13:25 +0530

    test

---
+++ Js_practice/JS_PART2/DOMassignment3/data.js
@@ -0,0 +1,15 @@
+
+function changecolor(){
+    let one=document.getElementsByTagName('button')[0]
+    console.log(one)
+    one.style.backgroundColor='red  '
+    one.style.width='100px'
+    one.style.height='20px'
+}
+function one(){
+    let one=document.getElementsByTagName('button')[0]
+    console.log(one)
+    one.innerHTML="GOOD MORNING"
+    one.style.width='100px'
+    one.style.height='20px'
+}
---
+++ Js_practice/JS_PART2/DOMassingment4/data.js
@@ -0,0 +1,12 @@
+function one(){
+    let one=document.getElementById('abc')
+    console.log(one)
+    one.style.backgroundColor='red'
+}
+
+function changecase(){
+    let two=document.getElementById('123')
+    // console.log(one)
+    let uname=two.value
+    two.value=uname.toUpperCase()
+}
