0580d7abb717f12a4f480ddbad46ba7afc5b8f6d
siva krishna <sivakrishna053936@gmail.com>
2024-07-22 20:17:41 +0530

    te

---
+++ Js_practice/JS_PART3/OOP-1/three.js
@@ -0,0 +1,12 @@
+class Account{
+    min_bal=500;
+    open_account(){
+        console.log("open acoount successfully")
+    }
+    deposite_amount(){
+        console.log("amount deposited successfully")
+    }
+}
+let a1=new Account
+a1.open_account();
+a1.deposite_amount()
