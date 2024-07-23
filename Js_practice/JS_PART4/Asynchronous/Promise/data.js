bfc1316c11287be812f050b3e2149be9cccc11a5
siva krishna <sivakrishna053936@gmail.com>
2024-07-23 16:48:44 +0530

    test

--- Js_practice/JS_PART4/Asynchronous/Promise/data.js
+++ Js_practice/JS_PART4/Asynchronous/Promise/data.js
@@ -1,12 +1,34 @@
 let employees=[{id:101,name:"Rahul",sal:46000},
     {id:102,name:"Sonia",sal:54000},
 ]
-let Create_employee=(emp)=>{
+let createEmployee=(emp)=>{
       return new Promise((resolve,reject)=>{
-              setTimeout(()=>{
-                 let flag=true
-                 flag? resolve("Data inserted"):reject("Failed")
-                 employees.push(emp)
-            },4000)
+               setTimeout(()=>{
+                  let flag=true
+                  flag?resolve("data inserted"):reject("failed")
+                  employees.push(emp)
+               },4000)
       })
 }
+
+let displaydata=()=>{
+      setTimeout(()=>{
+            let rows=""
+            for(user of employees){
+                  rows=rows+`<tr>
+                  <td>${user.id}</td>
+                  <td>${user.name}</td>
+                  <td>${user.sal}</td>
+                  </tr>`
+            }
+            document.getElementById('abc').innerHTML=rows
+      },2000)
+}
+createEmployee({id:103,name:"mango",sal:78945})
+.then((msg)=>{
+      console.log(msg)
+      displaydata()
+})
+.catch((err)=>{
+ console.log(err)
+})
