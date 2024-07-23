4ed44b06423b9dc457a57dbbea5b694af9bfb1bb
siva krishna <sivakrishna053936@gmail.com>
2024-07-23 12:56:26 +0530

    test

--- Js_practice/JS_PART3/OOP-4/Account.js
+++ Js_practice/JS_PART3/OOP-4/Account.js
@@ -25,3 +25,20 @@ class SA extends Account{
 let s1=new SA(101,"Siva","siva053023@gmail.com",50000,"podalakur")
 console.log(s1)
 console.log(s1.get_bal())
+
+class CA extends Account{
+    acc_id;
+    acc_bal=0
+    min_bal=25000
+    constructor(id,name,mail,amount,addr){
+        super(id,name,mail)
+        this.acc_id=id
+        this.acc_bal=amount
+    }
+    get_bal(){
+        return this.acc_bal-this.min_bal
+    }
+}
+let ca1=new CA(102,"Krishna","krishna053@gmail.com",80000,"New_Delhi")
+console.log(ca1)
+console.log(ca1.get_bal())
