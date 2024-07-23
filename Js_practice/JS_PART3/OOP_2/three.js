366b4331ac9a58d1ae8ead426bc44229d7907593
siva krishna <sivakrishna053936@gmail.com>
2024-07-23 10:52:39 +0530

    test

--- Js_practice/JS_PART3/OOP_2/three.js
+++ Js_practice/JS_PART3/OOP_2/three.js
@@ -3,13 +3,13 @@ class Account{
     min_bal=500
 
     deposite_amount(amount){
-        this.acc_bal=this.acc_bal+amount
+        this.acc_bal=this.acc_bal+amount+  "amount deposited successfully"
     }
     get_bal(){
          return this.acc_bal-this.min_bal
     }
-    with_drawl(amount){
-        this.acc_bal=this.acc_bal-amount
+    with_drawl(mount){
+        this.acc_bal=this.acc_bal-mount
     }
 
 }
@@ -18,5 +18,5 @@ a1.deposite_amount(3500)
 console.log(a1)
 a1.get_bal()
 console.log(a1)
-a1.with_drawl(5600)
+a1.with_drawl(1600)
 console.log(a1)
