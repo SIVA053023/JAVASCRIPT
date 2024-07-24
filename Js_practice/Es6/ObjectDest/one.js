534b3bf711ee08359e98697e6bca98e99f7a023b
siva krishna <sivakrishna053936@gmail.com>
2024-07-24 11:37:40 +0530

    test

---
+++ Js_practice/Es6/ObjectDest/one.js
@@ -0,0 +1,15 @@
+let emp={
+    id:101,
+    name:"Siva",
+    mail:"siva053@gmail.com"
+}
+console.log(emp.id)
+
+let users=[{ id:101,
+    name:"Siva",
+    mail:"siva053@gmail.com"},
+    {id:102,
+    name:"Krishna",
+    mail:"krishna053@gmail.com"}]
+
+    console.log(users[1].id)
---
+++ Js_practice/Es6/ObjectDest/two.js
@@ -0,0 +1,10 @@
+let emp={
+    id:101,
+    name:"Rahul Gandhi",
+    sal:45000,
+    loc:"Wayanad",
+    email:"rg@gmail.com"
+}
+let {id,sal}=emp
+console.log(id)
+console.log(sal)
